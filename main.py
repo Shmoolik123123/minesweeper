b985d7fc904c83c4f0781fd5240014bd19c4bb6a
eli meyer <96625700+Shmoolik123123@users.noreply.github.com>
2022-05-11 17:51:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from tkinter import Tk, Label, Button, Frame
+from tkinter.simpledialog import askstring
+
 from cell import Cell
 import settings
 import utils
 import sys
 import os
+
+name = askstring('Name', 'Welcome to minesweeper game, pleas enter your name')
 
 root = Tk()
 # override the settings of the window
@@ -12,13 +16,17 @@ root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.resizable(False, False)
 root.title("MINESWEEPER")
 
+game_version = 'V2'
+
 
 def restart_program():
-    """Restarts the current program.
-    Note: this function does not return. Any cleanup action (like
-    saving data) must be done before calling this function."""
+    # Any unsaved data will be lost after using this function.
     python = sys.executable
     os.execl(python, python, *sys.argv)
+
+
+def exit_to_desktop():
+    sys.exit()
 
 
 top_frame = Frame(
@@ -74,6 +82,43 @@ restart_btn.place(
     x=utils.width_prct(0.5), y=utils.height_prct(1)
 )
 
+exit_btn = Button(
+    top_frame,
+    command=exit_to_desktop,
+    bg='black',
+    fg='red',
+    activeforeground='black',
+    text='EXIT',
+    font=('', 20)
+)
+exit_btn.place(
+    x=utils.width_prct(0.5), y=utils.height_prct(10)
+)
+
+Version = Label(
+    top_frame,
+    bg='black',
+    fg='white',
+    text=f"Game version: {game_version}",
+    font=('', 25)
+)
+
+Version.place(
+    x=utils.width_prct(81.5), y=0
+)
+
+username_display = Label(
+    left_frame,
+    bg='black',
+    fg='white',
+    text=f"Welcome {format(name)}",
+    font=('', 25)
+)
+
+username_display.place(
+    x=0, y=utils.height_prct(10)
+)
+
 center_frame = Frame(
     root,
     bg='black',
@@ -104,3 +149,4 @@ Cell.randomize_mines()
 
 # run the window
 root.mainloop()
+
